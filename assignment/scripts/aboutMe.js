1f18bf5d9b3310dd91eabef866b1e1491799df4d
David Vetter <ddvetter23@gmail.com>
2022-07-27 11:28:53 -0500

    created name variables

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,10 +1,16 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 
+const firstName = 'David';
+
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+
+const lastName = 'Vetter';
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
+
+const fullName = firstName + lastName;
 
 // 4 - Console log the value of `fullName`
 
