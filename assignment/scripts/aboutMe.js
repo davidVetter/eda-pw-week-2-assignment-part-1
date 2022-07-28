03fd98bf5317209ba42503d001f52f9f128b9768
David Vetter <ddvetter23@gmail.com>
2022-07-28 09:44:23 -0500

    added compound conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -62,7 +62,9 @@ if (adventurous) {
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
-
+if (luckyNumber == 2 && adventurous) {
+    console.log('Roll the dice!');
+}
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
