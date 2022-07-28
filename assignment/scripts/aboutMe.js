f172efb4233115dd9c1054f2268e6a2523695d27
David Vetter <ddvetter23@gmail.com>
2022-07-28 10:03:50 -0500

    added else if conditional for pets

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -72,6 +72,14 @@ if (luckyNumber == 2 && adventurous) {
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
 
+if (pets < allowedPets) {
+    console.log('I can have more pets!');
+} else if (pets == allowedPets) {
+    console.log('I have enough pets');
+} else {
+    console.log('Oh no, I have to many pets!');
+}
+
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
